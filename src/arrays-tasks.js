3f56db3e10c95d78b83423c38bc60a832380aa12
Dromari <7709722@gmail.com>
2025-11-16 23:49:09 +0300

    feat: last of solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -519,8 +519,21 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => longest is [3, 10] and [1, 20] => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => longest is [7, 40, 80] => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  return nums.reduce(
+    (acc, item, index, array) => {
+      if (item > array[index - 1]) {
+        acc.current += 1;
+        if (acc.current > acc.maximum) {
+          acc.maximum = acc.current;
+        }
+      } else {
+        acc.current = 1;
+      }
+      return acc;
+    },
+    { current: 1, maximum: 1 }
+  ).maximum;
 }
 
 /**
